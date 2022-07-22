99aa2b3882e647bc1b3d01482e7a5e94b015eb72
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-07-22 22:31:43 +0700

    Addition setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-gray': '#f6f7f9',
+        'primary-yellow': '#ffe14c',
+        'dark-gray': '#2a3751',
+      },
+      backgroundImage: {
+        'login-bg': "url('/src/assets/images/login_bg.svg')",
+      },
+    },
   },
   plugins: [],
 };
